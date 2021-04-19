6023c4746e30ee30eab8e5ae23d335984c268888
jackjack3006 <jvsanoj23@gmail.com>
2021-04-19 21:30:34 +0530

    We create a function called resize that helps in getting us to change the size of the window so that processing speed increases drastically and also use and and when required in a code if the file is too big and stuff

--- resizing.py
+++ resizing.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 
 
-def resize(frame, scale=0.75):
+def resize(frame, scale=0.75):          # The scale refers to the size wanted by the original size into 1
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
 
