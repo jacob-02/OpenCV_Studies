df70385cb57af4907c6f8dee1695e2851182e1d8
jackjack3006 <jvsanoj23@gmail.com>
2021-04-19 21:08:27 +0530

    Opening videos using OpenCV. We use infinite frames and consider each frame as a pic in a while loop while we play videos

---
+++ reading_videos.py
@@ -0,0 +1,9 @@
+import cv2 as cv
+capture = cv.VideoCapture('/home/jacob3006/Videos/st23_naruto-shippuuden-dub-episode-476.1618454201.mp4')  #We can also use integers instead of the path file. Those reference cameras.
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Anime', frame)
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+capture.release()
+cv.destroyAllWindows()
