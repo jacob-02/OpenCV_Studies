5e8463b9a5d87e4ed68095a0c5553f785b5564c2
jackjack3006 <jvsanoj23@gmail.com>
2021-04-20 21:22:03 +0530

    In this we learn to move the image in the window and also how to rotate an image in a window

--- transformations.py
+++ transformations.py
@@ -2,11 +2,27 @@ import cv2 as cv
 import numpy as np
 
 
-def translate(image, x, y):
+# translation
+def translate(img, x, y):
     transMat = np.float32([[1, 0, x], [0, 1, y]])
-    dimensions = (image.shape[1], image.shape[0])
+    dimensions = (img.shape[1], img.shape[0])
 
-    return cv.warpAffine(image, transMat, dimensions)
+    return cv.warpAffine(img, transMat, dimensions)
+
+
+# rotation
+
+def rotate(img, angle, rotPoint=None):
+    (height, width) = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width // 2, height // 2)
+
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, scale=1.0)
+
+    dimensions = (width, height)
+
+    return cv.warpAffine(img, rotMat, dimensions)
 
 
 # negative values of x translates to the left
@@ -15,7 +31,9 @@ def translate(image, x, y):
 image = cv.imread('/home/jacob3006/Pictures/Screenshot from 2021-03-28 23-13-03.png')
 
 translated = translate(image, 1000, 100)
-
 cv.imshow('Translated', translated)
+
+rotated = rotate(image, 45)
+cv.imshow('Rotated', rotated)
 
 cv.waitKey(0)
