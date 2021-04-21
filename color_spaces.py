3008a50ea4f528fc5d173b08ffc8d1f7a955b3ab
jackjack3006 <jvsanoj23@gmail.com>
2021-04-21 19:31:51 +0530

    Converting from one color scale to another

--- color_spaces.py
+++ color_spaces.py
@@ -1,8 +1,13 @@
 import cv2 as cv
+import matplotlib.pyplot as plt
 
 image = cv.imread('/home/jacob3006/Pictures/Screenshot from 2021-03-28 23-13-03.png')
 cv.imshow('Anime', image)
 
+plt.imshow(image)
+plt.show()
+
+# Direct conversions from one scale to another without converting it into bgr in between is not possible
 # Converting to greyscale
 
 grey = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
@@ -17,5 +22,11 @@ cv.imshow('HSV', hsv)
 
 lab = cv.cvtColor(image, cv.COLOR_BGR2Lab)
 cv.imshow("LAB", lab)
+
+# Showing the images in RGB
+
+rgb = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+cv.imshow("RGB", rgb)
+
 
 cv.waitKey(0)
