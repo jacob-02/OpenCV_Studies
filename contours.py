6977b597ef68978ec024a38a7b223966be33bb45
jackjack3006 <jvsanoj23@gmail.com>
2021-04-21 11:22:53 +0530

    Finding the contours in the image using greyscale and using findContours function

--- contours.py
+++ contours.py
@@ -1,6 +1,8 @@
 import cv2 as cv
+import numpy as np
 
 image = cv.imread('/home/jacob3006/Pictures/Screenshot from 2021-03-28 23-13-03.png')
+blank = np.zeros(image.shape, dtype='uint8')
 
 cv.imshow('Anime', image)
 
@@ -16,5 +18,8 @@ cv.imshow('Threshold', threshold)
 
 contours = cv.findContours(threshold, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 print("Number of contours is ", len(contours))
+
+cv.drawContours(blank, contours, -1, (0, 0, 255), 2)
+cv.imshow('Contours Drawn', blank)
 
 cv.waitKey(0)
