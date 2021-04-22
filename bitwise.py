992922784306a6a98d8ae0e1ecb158b61c01e7ad
jackjack3006 <jvsanoj23@gmail.com>
2021-04-22 20:18:30 +0530

    Using bitwise operations on images

---
+++ bitwise.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+import numpy as np
+
+blank = np.zeros((400, 400), dtype='uint8')
+
+rectangle = cv.rectangle(blank.copy(), (30, 30), (370, 370), 255, -1)
+
+circle = cv.circle(blank.copy(), (200, 200), 200, 255, -1)
+
+cv.imshow('Rectangle', rectangle)
+cv.imshow('Circle', circle)
+
+# BITWISE And
+
+bitwise_and = cv.bitwise_and(circle, rectangle)
+cv.imshow('AND', bitwise_and)
+
+# BITWISE Or
+
+bitwise_or = cv.bitwise_or(circle, rectangle)
+cv.imshow('OR', bitwise_or)
+
+cv.waitKey(0)
