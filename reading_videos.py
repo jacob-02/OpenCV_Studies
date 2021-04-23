ee8c0c6d8043cbfe3c54ded8889f2ed2de192820
jackjack3006 <jvsanoj23@gmail.com>
2021-04-23 09:51:59 +0530

    Watching videos on Open CV

--- reading_videos.py
+++ reading_videos.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 
-capture = cv.VideoCapture(0)  # We can also use integers instead of the path file. Those reference cameras.
+capture = cv.VideoCapture(
+    '/home/jacob3006/Videos/st23_naruto-shippuuden-dub-episode-476.1618454201.mp4')  # We can also use integers instead of the path file. Those reference cameras.
 while True:
     isTrue, frame = capture.read()
     cv.imshow('Anime', frame)
