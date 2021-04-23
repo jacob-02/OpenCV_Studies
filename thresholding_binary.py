98f65c057051e2067b9382bec84fb78b8b1c25c9
jackjack3006 <jvsanoj23@gmail.com>
2021-04-23 21:11:04 +0530

    Thresholding and Inverse Thresholding for now. We use adaptive methods, which are honestly better and also hardcoded methods

--- thresholding_binary.py
+++ thresholding_binary.py
@@ -14,4 +14,12 @@ cv.imshow('Threshold', thresh)
 threshold1, thresh1 = cv.threshold(grey, 225, 225, cv.THRESH_BINARY_INV)
 cv.imshow('Inverse Thresholding', thresh1)
 
+# Adaptive Thresholding
+adaptive_thresh = cv.adaptiveThreshold(grey, 155, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, blockSize=21, C=1)   # Computes the optimal thresholding value to average of the surrounding pixels. Kinda like your blurring
+cv.imshow('Adaptive Thresholding', adaptive_thresh)
+
+# Gaussian Thresholding
+gaussian_thresh = cv.adaptiveThreshold(grey, 155, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, blockSize=21, C=1)   # Computes the optimal thresholding value to average of the surrounding pixels of their weight. Kinda like your blurring
+cv.imshow('Gaussian Adaptive Thresholding', gaussian_thresh)
+
 cv.waitKey(0)
