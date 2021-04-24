5dd9f618510cc20071434e57d985b18040556fef
jackjack3006 <jvsanoj23@gmail.com>
2021-04-24 21:56:17 +0530

    To find the number of faces in an image and in a video and also to show which where the faces detected. Also we don't have min thingy in our detecMultiScale function

--- facerecog_hardcascading.py
+++ facerecog_hardcascading.py
@@ -1,11 +1,8 @@
 import cv2 as cv
 
 image = cv.imread('/home/jacob3006/Pictures/My Photos/IMG-20200207-WA0146.jpg')
-
 grey = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-
 haar_cascade = cv.CascadeClassifier('haar_face.xml')    # Reads the lines of code and stores in the variable declared
-
 faces_rect = haar_cascade.detectMultiScale(grey, scaleFactor=1.8)   # This scale factor is perfect. Tested for more than one image
 
 print('Number of people ', len(faces_rect))
@@ -14,18 +11,15 @@ for (x, y, w, h) in faces_rect:
     cv.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
 
 cv.imshow('Detected Faces', image)
-
 cv.waitKey(0)
 
-capture = cv.VideoCapture(0)
 
+capture = cv.VideoCapture(0)
 haar_cascade = cv.CascadeClassifier('haar_face.xml')  # Reads the lines of code and stores in the variable declared
 
 while True:
     isTrue, frames = capture.read()
-
     grey = cv.cvtColor(frames, cv.COLOR_BGR2GRAY)
-
     faces_rect = haar_cascade.detectMultiScale(grey,
                                                scaleFactor=1.6)  # This scale factor is perfect. Tested for more than one image
 
