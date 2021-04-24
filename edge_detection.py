2c1f7ad7c4236ce983c00dd2b811ae15ea252059
jackjack3006 <jvsanoj23@gmail.com>
2021-04-24 12:40:35 +0530

    Edge Detection using Lapacion Method

--- edge_detection.py
+++ edge_detection.py
@@ -1,6 +1,15 @@
 import cv2 as cv
+import numpy as np
 
-image = cv.imread('/home/jacob3006/Pictures/Screenshot from 2021-04-04 23-41-07.png')
+image = cv.imread('/home/jacob3006/PycharmProjects/OpenCV_Studies/Images/img.png')
 cv.imshow('Image', image)
+
+grey = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+# Laplacion Method
+lap = cv.Laplacian(grey, cv.CV_64F)
+lap = np.uint8(np.absolute(lap))
+
+cv.imshow('Laplacian', lap)
 
 cv.waitKey(0)
