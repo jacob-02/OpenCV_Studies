6893a24fb189c4ba597a9517b77dc3b9f6ffab17
jackjack3006 <jvsanoj23@gmail.com>
2021-04-24 21:38:48 +0530

    Detecting the number of faces in the image and in video and drawing rectangular boxes around the said faces

--- facerecog_hardcascading.py
+++ facerecog_hardcascading.py
@@ -2,16 +2,19 @@ import cv2 as cv
 import numpy as np
 
 image = cv.imread('/home/jacob3006/Pictures/My Photos/IMG-20200207-WA0146.jpg')
-cv.imshow('Person', image)
 
 grey = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-cv.imshow('Grey', grey)
 
 haar_cascade = cv.CascadeClassifier('haar_face.xml')    # Reads the lines of code and stores in the variable declared
 
 faces_rect = haar_cascade.detectMultiScale(grey, scaleFactor=1.8)   # This scale factor is perfect. Tested for more than one image
 
 print('Number of people ', len(faces_rect))
+
+for (x, y, w, h) in faces_rect:
+    cv.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
+
+cv.imshow('Detected Faces', image)
 
 cv.waitKey(0)
 
@@ -21,15 +24,18 @@ haar_cascade = cv.CascadeClassifier('haar_face.xml')  # Reads the lines of code 
 
 while True:
     isTrue, frames = capture.read()
-    cv.imshow('WebCam', frames)
 
     grey = cv.cvtColor(frames, cv.COLOR_BGR2GRAY)
-    cv.imshow('Grey', grey)
 
     faces_rect = haar_cascade.detectMultiScale(grey,
                                                scaleFactor=1.8)  # This scale factor is perfect. Tested for more than one image
 
     print('Number of people ', len(faces_rect))
+
+    for (x, y, w, h) in faces_rect:
+        cv.rectangle(frames, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
+
+    cv.imshow('Detected Faces', frames)
 
     if cv.waitKey(20) & 0xFF == ord('d'):
         break
