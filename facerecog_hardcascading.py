15a75a4d2229475285fb88769cc56892065f4bd6
jackjack3006 <jvsanoj23@gmail.com>
2021-04-24 21:25:19 +0530

    Detecting the number of faces in the image

--- facerecog_hardcascading.py
+++ facerecog_hardcascading.py
@@ -1,7 +1,16 @@
 import cv2 as cv
 import numpy as np
 
-image = cv.imread('Images/img.png')
-cv.imshow('Cat', image)
+image = cv.imread('/home/jacob3006/Pictures/My Photos/IMG-20200207-WA0537.jpg')
+cv.imshow('Person', image)
+
+grey = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+cv.imshow('Grey', grey)
+
+haar_cascade = cv.CascadeClassifier('haar_face.xml')    # Reads the lines of code and stores in the variable declared
+
+faces_rect = haar_cascade.detectMultiScale(grey, scaleFactor=1.8)
+
+print('Number of people ', len(faces_rect))
 
 cv.waitKey(0)
