fe7e0b5eeb3318ec3b836fe5d47e4e710fb1f861
jackjack3006 <jvsanoj23@gmail.com>
2021-04-24 18:35:42 +0530

    Edge Detection using Laplacian Method and also using Sobel Method

--- edge_detection.py
+++ edge_detection.py
@@ -1,3 +1,4 @@
+import cv2
 import cv2 as cv
 import numpy as np
 
@@ -6,10 +7,24 @@ cv.imshow('Image', image)
 
 grey = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
-# Laplacion Method
+# Laplacian Method
 lap = cv.Laplacian(grey, cv.CV_64F)
 lap = np.uint8(np.absolute(lap))
 
 cv.imshow('Laplacian', lap)
+
+# Sobel
+sobelx = cv2.Sobel(grey, cv.CV_64F, 1, 0)
+sobely = cv2.Sobel(grey, cv.CV_64F, 0, 1)
+
+combined_sobel = cv.bitwise_or(sobelx, sobely)
+
+cv.imshow('SobelX', sobelx)
+cv.imshow('SobelY', sobely)
+
+cv.imshow('Combined Sobel', combined_sobel)
+
+canny = cv.Canny(grey, 150, 175)
+cv.imshow('Canny', canny)
 
 cv.waitKey(0)
